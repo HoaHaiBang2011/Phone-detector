fe6454002abfdb31c5c843dfcedb5cad29f10c6b
HoaHaiBang2011 <hoahaibang2021@gmail.com>
2026-04-27 20:13:49 +0700

    Update buzzer_controller.h

--- main/buzzer_controller.h
+++ main/buzzer_controller.h
@@ -1,20 +1,21 @@
 #ifndef BUZZER_CONTROLLER_H
 #define BUZZER_CONTROLLER_H
 
-#include <stdint.h>
+#include <Arduino.h>
 #include "config.h"
 
 class BuzzerController {
 public:
     static void init();
-    static void play(int8_t rssi);
+    static void setTargetRSSI(int8_t rssi); // Nhận RSSI từ target
+    static void update();                  // Gọi trong loop() để xử lý nhịp bíp
     static void stop();
-    static void setFrequency(uint16_t freq);
-    static void setDutyCycle(uint8_t duty);
     
 private:
-    static uint16_t current_freq;
-    static uint8_t current_duty;
+    static int8_t  _target_rssi;
+    static uint32_t _last_beep_ms;
+    static bool    _is_beeping;
+    static bool    _enabled;
 };
 
-#endif // BUZZER_CONTROLLER_H
+#endif
