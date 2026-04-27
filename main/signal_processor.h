b44ceec42f17c392b14d52bae71417a6005792d6
HoaHaiBang2011 <hoahaibang2021@gmail.com>
2026-04-27 20:51:04 +0700

    Update signal_processor.h

--- main/signal_processor.h
+++ main/signal_processor.h
@@ -9,7 +9,7 @@ public:
     // Cập nhật RSSI và tính toán lại giá trị trung bình ngay lập tức
     static void updateRSSI(WiFiDevice_t* device, int8_t new_rssi);
     
-    // Lấy RSSI trung bình (đã được tính sẵn, không tốn CPU để tính lại)
+    // Lấy RSSI trung bình đã được tính sẵn
     static int8_t getAverageRSSI(const WiFiDevice_t* device);
     
     static uint8_t rssiToPercentage(int8_t rssi);
